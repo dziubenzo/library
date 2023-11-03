7e1994d3022e603d0c10725e8cd6522b65bb7879
dziubenzo <dziubenzo@gmail.com>
2023-11-03 17:11:06 +0100

    Display books from library array on page

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
-const books = [];
+const library = [];
+const booksDiv = document.querySelector('.books');
 
 function Book(title, author, pages, read) {
   this.title = title;
@@ -7,8 +8,43 @@ function Book(title, author, pages, read) {
   this.read = Boolean(read);
 }
 
-books.push(new Book('Ślepnąć od świateł', 'Jakub Żulczyk', 520, true));
-books.push(new Book('How to Win at Chess', 'Levy Rozman', 272, false));
-books.push(new Book('Historia bez cenzury', 'Wojciech Drewniak', 288, true));
+library.push(new Book('Ślepnąć od świateł', 'Jakub Żulczyk', 520, true));
+library.push(new Book('How to Win at Chess', 'Levy Rozman', 272, false));
+library.push(new Book('Historia bez cenzury', 'Wojciech Drewniak', 288, true));
 
-console.table(books);
+function displayBooks() {
+  let index = 0;
+  for (book of library) {
+    const card = document.createElement('div');
+    card.setAttribute('class', 'card');
+    card.setAttribute('data-book-id', `${index}`);
+    booksDiv.appendChild(card);
+    const title = document.createElement('p');
+    title.setAttribute('class', 'title');
+    title.textContent = book.title;
+    card.appendChild(title);
+    const author = document.createElement('p');
+    author.setAttribute('class', 'author');
+    author.textContent = book.author;
+    card.appendChild(author);
+    const pages = document.createElement('p');
+    pages.setAttribute('class', 'pages');
+    pages.textContent = book.pages;
+    card.appendChild(pages);
+    const read = document.createElement('p');
+    read.setAttribute('class', 'read');
+    if (book.read) {
+      read.textContent = 'Read';
+    } else {
+      read.textContent = 'Not read';
+    }
+    card.appendChild(read);
+    const deleteIcon = document.createElement('img');
+    deleteIcon.setAttribute('src', './css/delete-icon.svg');
+    deleteIcon.setAttribute('alt', 'Delete Icon');
+    card.appendChild(deleteIcon);
+    index++;
+  }
+}
+
+displayBooks();
